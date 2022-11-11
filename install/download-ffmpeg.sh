80ecbc335395ce0998b33274ef684c04bc789680
Timothée Gauthier <2079042+Hettomei@users.noreply.github.com>
2022-11-11 14:52:23 +0100

    Start to download ffmpeg

--- install/download-ffmpeg.sh
+++ install/download-ffmpeg.sh
@@ -1,4 +1,8 @@
 cd git rev-parse --show-toplevel
 
+
 echo "from https://johnvansickle.com/ffmpeg/"
+echo "only for ubuntu amd 64"
+
+cd bin
 wget "https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz"
